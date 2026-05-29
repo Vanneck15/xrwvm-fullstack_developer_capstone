dea7a52913d8b6f9bf1db3fde3796fb2562d6a24
Vanneck15 <vanneckgueb@gmail.com>
2026-05-28 21:03:51 -0400

    Ajout des vues concessionnaires, gestion des avis et intégration de l'analyse de sentiment

--- server/server/djangoapp/views.py
+++ server/server/djangoapp/views.py
@@ -6,9 +6,11 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-# Importation des modèles et de la fonction pour peupler la base de données
+# IMPORTATIONS SUPPLÉMENTAIRES
 from .models import CarMake, CarModel
 from .populate import initiate
+# Ajout de post_review pour l'envoi des avis
+from .restapis import get_request, analyze_review_sentiments, post_review  
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -31,12 +33,12 @@ def registration(request):
         logger.debug("{} is new user".format(username))
 
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -66,4 +68,62 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
+
+
+# --------------------------------------------------------------------------
+# VUES POUR LES CONCESSIONNAIRES AND LES CRITIQUES
+# --------------------------------------------------------------------------
+
+# Vue pour obtenir tous les concessionnaires (ou par État si spécifié)
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/" + state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+
+# Vue pour obtenir les détails d'un concessionnaire spécifique via son ID
+def get_dealer_details(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealer = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealer})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+
+# Vue pour obtenir les critiques d'un concessionnaire et analyser leur sentiment
+def get_dealer_reviews(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            # Appel au microservice d'analyse de sentiment
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+
+# Vue pour ajouter une critique (Review) sur un concessionnaire
+@csrf_exempt
+def add_review(request):
+    if (request.user.is_authenticated == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200})
+        except Exception as err:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
+    else:
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200})
+        except Exception as err:
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
